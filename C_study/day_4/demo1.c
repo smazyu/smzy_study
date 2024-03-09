d25cbe6cd69157797581993a491866117a3ee112
毓 <2021223020@qq.com>
2024-03-09 19:17:29 +0800

    study

---
+++ C_study/day_4/demo1.c
@@ -0,0 +1,36 @@
+//
+// Created by 20212 on 3/9/2024.
+//函数
+//f(x) = 2x + 1;
+//c语言中的函数也是一样
+//f(x,x) = x + y;
+//#include <stdio.h>
+////函数的定义
+//int Add(int x,int y){
+//    int z = x + y;
+//    return z;
+//}
+////函数为代码的一个模块化，可以重复使用,提高代码的复用性
+////函数必须含有参数
+//int main(){
+//    int num1 = 10;
+//    int num2 = 20;
+//    scanf("%d %d",&num1,&num2);
+////    int sum = num1 + num2;
+////    printf("sum = %d\n",sum);
+//    int num = Add(num1,num2);
+//    printf("num = %d\n",num);
+//}
+//一组相同类型的集合 数组
+//数组是由下标来访问
+#include <stdio.h>
+//int main(){
+//    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//    char ch[5] = {'a','b','c'};//不完全初始化,剩余的默认为零
+//    int i = 0;
+//    while (i<10){
+//        printf("%d\n",arr[i]);
+//        i++;
+//    }
+//}
+
