60a252cdf8aae1bc25e89f5202ef06120d2e86f6
毓 <2021223020@qq.com>
2024-03-11 00:11:36 +0800

    c_study

--- C_study/day_5/study.c
+++ C_study/day_5/study.c
@@ -43,6 +43,136 @@
 //    return 0;
 //}
 //
+#include <stdio.h>
+//int main(){
+//    for(int i = 1;i <= 100;i+=2){
+//        if(i % 2 ==1){
+//        printf("i = %d\n",i);}
+//    }
+//}
+
+//
+//swtich用于多分支环境
+//int main(){
+//    int day = 0;
+//    &取地址符,当你使用 scanf("%d", &day); 时，你实际上是告诉 scanf 函数将输入的整数存储在 day 变量的内存地址中
+//当你int 初始化一个变量时，会给这个变量分配一个内存地址
+//scanf是将输入的值赋值给变量,本质是把这个值放到变量的内存地址中
+//swich语句中的case后面的值必须是常量
+//swich必须使用整型或者字符型,因为字符型底层是ASCII码,也是整型
+//default是可选的,默认选项,当所有的case都不满足时,执行default
+//    scanf("%d",&day);
+//    switch(day){
+//        case 1:
+//            printf("星期一\n");
+//            break;
+//        case 2:
+//            printf("星期二\n");
+//            break;
+//        case 3:
+//            printf("星期三\n");
+//            break;
+//        case 4:
+//            printf("星期四\n");
+//            break;
+//        case 5:
+//            printf("星期五\n");
+//            break;
+//        case 6:
+//            printf("星期六\n");
+//            break;
+//        case 7:
+//            printf("星期日\n");
+//            break;
+//        default:
+//            printf("输入错误\n");
+//            break;
+//    }
+//}
+
+//switch允许嵌套
+//int main(){
+//    int n =1;
+//    int m = 2;
+//    switch (n){
+//        case 1:
+//            m++;
+//case 2:
+//    n++;
+//    case 3:
+//        switch(n){
+//            case 1:
+//                n++;
+//            case 2:
+//                m++;
+//                break;
+//        }
+//    }
+//    printf("m = %d,n = %d\n",m,n);
+//}
+
+//
+//
+//
+//switch 中break是跳出当前的switch语句
+//int main(){
+//    int n = 1;
+//    int m = 2;
+//    switch(n){
+//        case 1:
+//            m++;
+//            break;
+//        case 2:
+//            n++;
+//            break;
+//        case 3:
+//            switch(n){
+//                case 1:
+//                    n++;
+//                case 2:
+//                    m++;
+//                    break;
+//            }
+//    }
+//    printf("m = %d,n = %d\n",m,n);
+//}
+
+//while 循环在while循环中break代表永久跳出循环 continue代表跳出本次循环
+//int main(){
+//    int i = 1;
+//    while(i <= 10){
+//        if (i ==5 ){
+//            printf("hello world %d\n",i);
+//            break;
+//        }
+//        printf("hello world %d\n",i);
+//        i ++;
+//    }
+//}
+//getchar 获取一个字符
+//  EOF -> end of file文件结束的标志
+//int main(){
+////    int ch = 0;
+////    int ch = getchar();
+////    while((ch = getchar()) != EOF){
+////    printf("%c\n",ch);
+////putchar(ch);
+////    }
+//    int ch = 0;
+//    while((ch = getchar() !=(-1))){
+//        putchar(ch);
+//    }
+//}
+//键盘和getchar之间有个缓存区,当键盘输入一个字符时,会先放到缓存区,当按下回车键时,getchar才会从缓存区取出一个字符
+int main(){
+    int ch = 0;
+    while((ch = getchar()) != EOF){
+        putchar(ch);
+    }
+}
+
+
+
 
 
 
