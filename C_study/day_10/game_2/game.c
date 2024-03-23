f9f180b3ce6cc026a7bdd8e4da81f677e59552eb
毓 <138857469+smazyu@users.noreply.github.com>
2024-03-23 18:49:43 +0800

    c_study

--- C_study/day_10/game_2/game.c
+++ C_study/day_10/game_2/game.c
@@ -3,3 +3,40 @@
 //
 
 #include "game.h"
+void InitBoard(char board[ROWS][COLS],int rows,int cols,char set){
+    int i = 0;
+    int j = 0;
+    for(i = 0;i < rows; i++){
+        for (j = 0;j < cols;j++){
+            board[i][j] = set;
+        }
+    }
+}
+void DisPlayBoard(char board[ROWS][COLS],int row,int col){
+    int i = 0;
+    int j = 0;
+    printf("-----扫雷游戏------\n");
+    for (i = 0;i<=col;i++){
+        printf( "%d ",i);
+    }
+    printf("\n");
+    for(i = 1;i <= row;i++){
+        printf("%d",i);//打印行
+        for(j = 1;j <= col;j++){
+            printf(" %c",board[i][j]);
+        }
+        printf("\n");
+    }
+}
+void SetMine(char mine[ROWS][COLS],int row,int col){
+//    布置十个雷
+    int count = 10;
+    while(count){
+        int x = rand()%row + 1;
+        int y = rand()%col + 1;
+        if(mine[x][y] == '0'){
+            mine[x][y] = '1';
+            count--;
+        }
+    }
+}
--- C_study/day_10/game_2/game.h
+++ C_study/day_10/game_2/game.h
@@ -2,3 +2,14 @@
 // Created by 20212 on 2024/3/22.
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+
+//≥ı ºªØ∆Â≈Ã
+void InitBoard(char board[ROWS][COLS],int rows,int cols,char set);
+void DisPlayBoard(char board[ROWS][COLS],int row,int col);
+void SetMine(char board[ROWS][COLS],int row,int col);
