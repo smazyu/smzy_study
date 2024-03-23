dee529a4984ef977732c2b2ecb5ce53a59ddae1a
毓 <138857469+smazyu@users.noreply.github.com>
2024-03-24 00:42:12 +0800

    c_study

---
+++ C_study/day_10/demo2.c
@@ -0,0 +1,15 @@
+//
+// Created by 20212 on 2024/3/24.
+//
+#include <stdio.h>
+int main(){
+//    int a = 3/5;
+    //c语言里面的操作符 / 整数除整数取整 只有一边有小数才会得到小数
+//    printf("%d",a);
+//    int a = 7 % 3;//取模必须是正数
+//    printf("%d",a);
+    int a = 2;
+    int b = a << 1;
+    //把a的二进制位左移一位
+    printf("%d",b);
+}
