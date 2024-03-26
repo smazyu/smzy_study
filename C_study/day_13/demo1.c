3090ec299f84900bc53ba49597c81c8ceece3ee6
毓 <138857469+smazyu@users.noreply.github.com>
2024-03-26 23:42:11 +0800

    c_study

--- C_study/day_13/demo1.c
+++ C_study/day_13/demo1.c
@@ -8,16 +8,31 @@
 ////    数组名是数组首元素的地址
 //    printf("%p\n",&arr[0]);
 //}
+//int main(){
+//    int arr[10] = {0};
+//    int*p = arr;
+//    int i = 0;
+//    for(i;i<10;i++){
+//        printf("%p==>%p\n",&arr[i],p+i);
+//        *(p+i) = i;
+////        arr[2] == *(p+2)  ==  *(2+p) == *(2+arr)
+//    }
+//    for(i=0;i<10;i++){
+//        printf("%d\n",*(p+i));
+//    }
+//}
+//int main(){
+//    int a = 10;
+//    int * pa = &a;
+//    int ** ppa = &pa;
+//    //ppa就是一个二级指针变量
+//    return 0;
+//}
+//指针数组 - 数组
 int main(){
-    int arr[10] = {0};
-    int*p = arr;
-    int i = 0;
-    for(i;i<10;i++){
-        printf("%p==>%p\n",&arr[i],p+i);
-        *(p+i) = i;
-//        arr[2] == *(p+2)  ==  *(2+p) == *(2+arr)
-    }
-    for(i=0;i<10;i++){
-        printf("%d\n",*(p+i));
-    }
+    int arr[10];//整形数组 - 存放整形的数组叫做整形数组
+    char ch[5];//字符数组
+    int * parr[5 ];
+    return 0;
+
 }
