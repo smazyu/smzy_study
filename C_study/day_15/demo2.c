02225c96581632fa8a874659b11bc424ce093d19
毓 <138857469+smazyu@users.noreply.github.com>
2024-03-30 02:20:44 +0800

    c_study

--- C_study/day_15/demo2.c
+++ C_study/day_15/demo2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int my_strlen(char * p){
+#include <assert.h>
+int my_strlen(const char * p){
+    assert(p);//p为空时，为空指针，为假，报错
     int num = 0;
     while(*p++ && ++num){
         ;
@@ -7,7 +9,8 @@ int my_strlen(char * p){
     return num;
 }
 int main(){
-    char arr1[] = "smzy";
+    char arr1[] = "";
     int num = my_strlen(arr1);
-    printf("%d\n",num);
+    // printf("%d\n",num);
+    printf("%s","hello");
 }
