68d42eb665cc101ab4f5852023b9e00f715a534c
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-07 21:55:26 +0800

    c_study

--- C_study/day_21/指针进阶4.c
+++ C_study/day_21/指针进阶4.c
@@ -1,3 +1,9 @@
 //
 // Created by 20212 on 2024/4/7.
 //
+void(* signal(int,void(*)(int)))(int);
+//1.signal 和 （） 相结合，说明signal是函数名
+//2.signal函数的第一个参数类型是int，第二个参数的类型是函数指针
+//该函数指针指向一个参数为int，返回类型是void的函数
+//3.signal函数的返回类型也是一个函数指针
+//该函数指针指向一个参数为int，返回类型为void的函数
