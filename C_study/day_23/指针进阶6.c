4bc1b6fa49c65c6e257e31a04571bd178acb254e
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-11 00:46:20 +0800

    c_study

--- C_study/day_23/指针进阶6.c
+++ C_study/day_23/指针进阶6.c
@@ -2,14 +2,23 @@
 // Created by 20212 on 2024/4/10.
 //
 //10 -> 9
-vold bubble_sort(int arr[],int sz){
-    int i = 0;
-     for(i = 0;i < sz - 1;i++){
-//   一趟冒泡排序
-     }
-}
-int main(){
-    int arr[10] = {9,2,3,6,1,0,6,3,12,34};
-    int sz = sizeof(arr) / sizeof([0]);
-    bubble_sort(arr,sz);
-}
+//
+//void bubble_sort(int arr[],int sz){
+//    int i = 0;
+//     for(i = 0;i < sz - 1;i++){
+////   一趟冒泡排序
+//        int j = 0;
+//        for(j = 0;j < sz -1 -i;j++){
+//            if(arr[j] > arr[j + 1]){
+//                int temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//            }
+//        }
+//     }
+//}
+//int main(){
+//    int arr[10] = {9,2,3,6,1,0,6,3,12,34};
+//    int sz = sizeof(arr) / sizeof(arr[0]);
+//    bubble_sort(arr,sz);
+//}
