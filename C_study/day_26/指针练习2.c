1ba00da39197d2cdbbb5a336c21b67f6866b93a1
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-17 00:42:59 +0800

    c_study

--- C_study/day_26/指针练习2.c
+++ C_study/day_26/指针练习2.c
@@ -1,3 +1,14 @@
 //
 // Created by 20212 on 24-4-16.
 //
+//指针加一跟指针类型有关
+#include <stdio.h>
+//int main(){
+//   int a[4] = {1,2,3,4};
+//   int* ptr1 = (int*) (&a +1);
+//   int *ptr2 = (int*)((int)a + 1);
+//}
+int main(){
+    int a[3][2] = {(1,2),(1,2)};
+    printf("%d",a[0][0]);
+}
