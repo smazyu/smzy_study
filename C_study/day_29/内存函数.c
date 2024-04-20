31b5e738b13384e5078b8cbb8ee106b3d94c5261
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-21 01:14:14 +0800

    c_study

--- C_study/day_29/内存函数.c
+++ C_study/day_29/内存函数.c
@@ -1,3 +1,18 @@
 //
 // Created by 20212 on 24-4-20.
 //
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main(){
+//    FILE*pf = fopen("test.txt","r");
+//    if (pf == NULL){
+//        perror("fopen");
+//    }
+// isdigit 如果是数字字符返回非0的字，如果不是返回0
+//    char ch = 'a';
+//    int ret = islower(ch);
+//tolower 转换成小写字符
+//    printf("%d\n",ret);
+    char arr[20] = {0};
+}
