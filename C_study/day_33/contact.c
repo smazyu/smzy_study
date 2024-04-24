110e29150c75c7d9d6aa9f76701df1b64270b1f2
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-25 02:09:26 +0800

    c_study

---
+++ C_study/day_33/contact.c
@@ -0,0 +1,47 @@
+//
+// Created by 20212 on 24-4-25.
+//
+
+#include "contact.h"
+void menu(){
+    printf("************************************");
+    printf("*******1.add ****** 2.del*********************");
+    printf("*******3.search*****4.modify*******************");
+    printf("*******5.sort ******6.print*********************");
+    printf("*******0.exit****************************");
+    printf("************************************");
+}
+enum Option{
+    EXIT,
+    ADD,
+    DEL,
+    SEARCH,
+    SORT,
+    PRINT
+};
+int main(){
+    int input = 0;
+    PeoInfo data[MAX];
+    int sz = 0;
+    do{
+        menu();
+        printf("请选择 ：->");
+        scanf("%d",&input);
+        switch(input){
+            case ADD:
+                //增加人的信息
+                AddContact();
+                break;
+            case DEL:
+                break;
+            case SEARCH:
+                break;
+            case SORT:
+                break;
+            case PRINT:
+                break;
+        }
+
+    }while(input != EXIT);
+    return 0;
+}
