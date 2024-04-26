048ee4a20cc9225af6c9dcc553d43ac95c79111f
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-27 02:32:40 +0800

    c_study

--- C_study/day_36/FlexibleArray.c
+++ C_study/day_36/FlexibleArray.c
@@ -1,3 +1,9 @@
 //
 // Created by 20212 on 2024/4/27.
 //
+//Flexible array The last element in the structure is allowed to be an array of unknown size
+
+struct s{
+    int n;//4
+    int a[];//unknown size
+};
