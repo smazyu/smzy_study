487d5004b1fe3d02c5d8035e8853740176704dd9
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-28 03:00:34 +0800

    c_study

--- C_study/day_36/FlexibleArray.c
+++ C_study/day_36/FlexibleArray.c
@@ -3,7 +3,15 @@
 //
 //Flexible array The last element in the structure is allowed to be an array of unknown size
 
+#include <malloc.h>
+
 struct s{
     int n;//4
     int a[];//unknown size
 };
+int main(){
+    //it is expected that the size of a is 10 integers
+    struct s* ps = (struct s*)(malloc(sizeof(struct s) + 10*sizeof(int)));
+    //flexiblearray carved in heap space
+    //Because you can increase or decrease space through realloc
+}
