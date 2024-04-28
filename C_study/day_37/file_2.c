c8030e41a7e9dee3d281710277e16480247713ec
毓 <138857469+smazyu@users.noreply.github.com>
2024-04-29 02:22:43 +0800

    c_study

--- C_study/day_37/file_2.c
+++ C_study/day_37/file_2.c
@@ -3,24 +3,44 @@
 //
 
 #include <stdio.h>
+//struct S{
+//    char arr[10];
+//    int num;
+//    float sc;
+//};
+//int main(){
+//    struct S s= {"abcdef",10,5.5f};
+////"../C_study/day_37/txt.data"
+//    FILE*pf = fopen("../C_study/day_37/txt.data","r");
+//    if(pf == NULL){
+//        perror("fopen");
+//        return 1;
+//    }
+////    fprintf(pf,"%s %d %f",s.arr,s.num,s.sc);
+//    fscanf(pf,"%s %d %f",s.arr,&(s.num),&(s.sc));
+//    printf("%s %d %f",s.arr,s.num,s.sc);
+//
+//    //program output file writing
+//    fclose(pf);
+//    pf = NULL;
+//    return 0;
+//}
 struct S{
     char arr[10];
     int num;
     float sc;
 };
 int main(){
-    struct S s= {"abcdef",10,5.5f};
-//"../C_study/day_37/txt.data"
+//    struct S s= {"abcdef",10,5.5f};
+    struct S s= {0};
+
     FILE*pf = fopen("../C_study/day_37/txt.data","r");
     if(pf == NULL){
         perror("fopen");
         return 1;
     }
-//    fprintf(pf,"%s %d %f",s.arr,s.num,s.sc);
-    fscanf(pf,"%s %d %f",s.arr,&(s.num),&(s.sc));
-
-    //program output file writing
-    fclose(pf);
-    pf = NULL;
-    return 0;
+//    fwrite(&s,sizeof(struct S),1,pf);
+    fread(&s,sizeof(struct S),1,pf);
+    printf("%s %d %f\n",s.arr,s.num,s.sc);
 }
+
