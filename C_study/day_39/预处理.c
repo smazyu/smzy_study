604c9baa08e26192f5e0d5bc16d6e98f6d6c9732
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-06 23:13:39 +0800

    c_study

--- C_study/day_39/预处理.c
+++ C_study/day_39/预处理.c
@@ -1,7 +1,13 @@
 //
 // Created by 20212 on 2024/5/1.
 //
-//程序环境与预处理
+//程序环境与预处理  program environment and preprocessing
 //翻译环境，使test.c -> test.exe
+//Translation environment
 //test.c contact.c common.c
-//test
+//test.c -> txt.exe 依赖于翻译环境 Dependent on the translation environment
+//编译（编译器） Compiler (Compiler) cl.exe  链接器(link.exe)
+//源文件通过编译 链接 最后形成可执行文件
+///The source files are compiled through a link and finally formed into an executable file
+
+
