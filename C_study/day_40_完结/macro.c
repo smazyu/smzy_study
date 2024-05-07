ac6218f538ca2b8039fcee3e8f0cb074edf3656f
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-08 00:52:07 +0800

    Update macro.c to include M macro and fix formatting in preprocessing.c

--- C_study/day_40_完结/macro.c
+++ C_study/day_40_完结/macro.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#define M 1
 int main(){
-    int a = 1;
+    int a = M;
     // int b = a + 1;
-    int b = ++a;//这里就叫做有副作用
-
+    printf("%d\n",a);
+#undef M
+    // int b = ++a;//这里就叫做有副作用
     //宏是直接替换
+    // int b = M 这里报错 因为我取消了宏定义
 }
 
- 
 //宏比函数的效率更高
 //宏可能不太严谨 宏运算优先级问题 
+
+//运算逻辑足够简单 用宏
+//不然就用函数
