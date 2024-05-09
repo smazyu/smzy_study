f61f9b21486ce560a4f1d65c684bb795335b33fe
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-10 03:39:56 +0800

    yy

---
+++ build/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smzy.dir/DataStruct/day_2/Seqlist.c.obj"
+  "CMakeFiles/smzy.dir/DataStruct/day_2/Seqlist.c.obj.d"
+  "CMakeFiles/smzy.dir/DataStruct/day_2/text.c.obj"
+  "CMakeFiles/smzy.dir/DataStruct/day_2/text.c.obj.d"
+  "libsmzy.dll.a"
+  "smzy.exe"
+  "smzy.exe.manifest"
+  "smzy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smzy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
