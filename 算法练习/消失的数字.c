9c0c9cf2eaa8544f5a04c9bcff887d29752939bf
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-09 17:55:03 +0800

    chore: Update CMakeLists.txt to include 消失的数字.c in the executable target

--- 算法练习/消失的数字.c
+++ 算法练习/消失的数字.c
@@ -1,6 +1,7 @@
 #if 0
 数组nums包含从0到n的所有整数，但其中缺了一个。请编写代码找出那个缺失的整数。你有办法在O(n)时间内完成吗？
 #endif
+#include <stdio.h>
 int missingNumber(int* nums, int numsSize){
     int i = 0;
     int num = 0;
@@ -9,5 +10,13 @@ int missingNumber(int* nums, int numsSize){
         num ^= i;
     }
     num ^= numsSize;
+    return num;
 }
-
+int main(){
+    int arr[] = {1,3,0};
+    int numsSize = sizeof(arr);
+    int num = missingNumber(arr,numsSize);
+    printf("%d\n",num);
+    return 0;
+}
+    
