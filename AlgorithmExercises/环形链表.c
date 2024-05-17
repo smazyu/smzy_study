74c9f4e283278148d461a08cd74fdd56178b7839
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-18 02:30:19 +0800

    feat: 添加判断链表是否有环的解决方案

---
+++ AlgorithmExercises/环形链表.c
@@ -0,0 +1,26 @@
+// 给你一个链表的头节点 head ，判断链表中是否有环。
+
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+
+// 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+#if 0
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode* head) {
+    struct ListNode *fast = head, *slow = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow) {
+            return true;
+        }
+    }
+    return false;
+}
+//如果x是奇数就会导致错过，不能相遇。如果错过之后，再算差距还是奇数。就会导致下次还是错过，如果极端情况下每次差距都是奇数，就会一直错过，无法相遇。再走n步，同理
+#endif
