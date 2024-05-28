f911299169e778a2c9a1aa4301ed4be0c0e1ab15
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-29 01:06:10 +0800

    y

--- DSStudy/day_9/stack/text.c
+++ DSStudy/day_9/stack/text.c
@@ -1,3 +1,25 @@
 //
 // Created by 20212 on 2024/5/27.
 //
+#include "stack.h"
+//栈的作用
+//1.如果有后进先出需求的地方
+//比如迷宫问题
+void TestStack(){
+    Stack st;
+    StackInit(&st);
+    StackPush(&st,1);
+    StackPush(&st,2);
+    StackPush(&st,3);
+    StackPush(&st,4);
+    while(!StackEmpty(&st)){
+        printf("%d",StackTop(&st));
+        StackPop(&st);
+    }
+    printf("\n");
+    StackDestory(&st);
+}
+int main(){
+    TestStack();
+    return 0;
+}
