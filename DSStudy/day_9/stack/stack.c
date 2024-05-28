16180fe9852e0740fd4fcb2bad30885d712568f2
毓 <138857469+smazyu@users.noreply.github.com>
2024-05-28 23:56:58 +0800

    y

--- DSStudy/day_9/stack/stack.c
+++ DSStudy/day_9/stack/stack.c
@@ -1,3 +1,15 @@
 //
 // Created by 20212 on 2024/5/27.
 //
+
+#include "stack.h"
+void StackInit(Stack *pst){
+    assert(pst);
+}
+void StackDestory(Stack *pst);
+//插入和取出
+void StackPush(Stack* pst,STDataType x);
+void StackPop(Stack* pst);
+int StackSize(Stack *pst);
+//返回1是空，返回0是非空
+int StackEmpty(Stack *pst);
--- DSStudy/day_9/stack/stack.h
+++ DSStudy/day_9/stack/stack.h
@@ -1,9 +1,21 @@
 #pragma once
 //栈也可以写成静态的
+#include <assert.h>
 typedef int STDataType;
 typedef struct Stack{
     STDataType * _a;
     int _top;//栈顶下标
     int _capacity;
 }Stack;
-void StactInit();
+//初始化和销毁
+void StackInit(Stack *pst);
+void StackDestory(Stack *pst);
+//插入和取出
+void StackPush(Stack* pst,STDataType x);
+void StackPop(Stack* pst);
+//数据个数
+int StackSize(Stack *pst);
+//返回1是空，返回0是非空
+int StackEmpty(Stack *pst);
+
+STDataType StackTop(Stack* pst);
