e91bee05bd793b39c54a7e599230ff9a5613ae10
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-09 21:53:54 +0800

    1

--- DSStudy/day_17/BinaryTree.c
+++ DSStudy/day_17/BinaryTree.c
@@ -1,6 +1,12 @@
 //
 // Created by 20212 on 6/7/2024.
 //
+//前序遍历 先根再左再右
+//中序遍历 先左再根再右
+//后序遍历 先左再右再中
+//前中后 深度优先遍历
+//通过递归定义
+//深度 就是访问方式先往深走
 #define DataType int
 typedef struct Node{
     DataType
