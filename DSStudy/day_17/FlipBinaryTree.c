72941005c7b3fe7d5b92c926eda9262633e7e0cb
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-15 21:38:42 +0800

    Update FlipBinaryTree.c

--- DSStudy/day_17/FlipBinaryTree.c
+++ DSStudy/day_17/FlipBinaryTree.c
@@ -1,3 +1,29 @@
 //
 // Created by 20212 on 15/7/2024.
 //
+//给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+
+#if 0
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if(root == NULL)
+    {    return NULL;
+    }
+    else{
+        struct TreeNode* temp = root -> left;
+        root -> left = root -> right;
+        root -> right = temp;
+
+        invertTree(root -> left);
+        invertTree(root -> right);
+        return root;
+    }
+}
+#endif
