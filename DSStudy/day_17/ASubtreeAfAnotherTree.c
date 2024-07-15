b4307b3245cd6e22af1221d701c191a6689efd41
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-16 00:34:23 +0800

    Update ASubtreeAfAnotherTree.c

--- DSStudy/day_17/ASubtreeAfAnotherTree.c
+++ DSStudy/day_17/ASubtreeAfAnotherTree.c
@@ -1,3 +1,24 @@
 //
 // Created by 20212 on 15/7/2024.
 //
+//给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
+//
+//二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。
+
+/**
+bool isSameTree(struct TreeNode* s, struct TreeNode* t) {
+    if (s == NULL && t == NULL)
+        return true;
+    if (s == NULL || t == NULL)
+        return false;
+    return (s->val == t->val) && isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
+}
+
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+    if (root == NULL)
+        return false;
+    if (isSameTree(root, subRoot))
+        return true;
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+}
+**/
