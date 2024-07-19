afbe2ca9d6691e635cb61eb9c9c510629e3b3e76
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-19 21:04:21 +0800

    1

--- DSStudy/day_18/BalancedBinaryTree.c
+++ DSStudy/day_18/BalancedBinaryTree.c
@@ -20,7 +20,11 @@ int TreeDepth(struct TreeNode* root) {
     int rightDepth = TreeDepth(root->right);
     return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
 }
+//高度被重复计算了
 
+//我们现在采用的是一种前序判断
+//导致计算高度存在大量的重复
+//如果我们使用后序判读 就可以解决这个问题
 bool isBalanced(struct TreeNode* root) {
     if (root == NULL)
         return true;
