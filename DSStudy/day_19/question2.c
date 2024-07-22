5e6b4d112dbb12d38ec06cfbf3a0a9152b5fa22c
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-23 00:34:12 +0800

    Update question2.c

--- DSStudy/day_19/question2.c
+++ DSStudy/day_19/question2.c
@@ -1,23 +1,32 @@
 //
 // Created by 20212 on 23/7/2024.
 //
+//销毁采用后序销毁
+//BTNode *BinaryTreeFind(BTNode*root,BTDataType x){
+//    if(root == NULL){
+//        return NULL;
+//    }
+//    if(root -> _data == x){
+//        return root;
+//    }
+//    BTNode* node = BinaryTreeFind(root -> _left,x){
+//        if(node){
+//            return node;
+//        }
+//    }
+//    BTNode* node = BinaryTreeFind(root -> _right,x){
+//        if(node){
+//            return node;
+//        }
+//    }
+//    return NULL;
+//}
 
-BTNode *BinaryTreeFind(BTNode*root,BTDataType x){
-    if(root == NULL){
-        return NULL;
-    }
-    if(root -> _data == x){
-        return root;
-    }
-    BTNode* node = BinaryTreeFind(root -> _left,x){
-        if(node){
-            return node;
-        }
-    }
-    BTNode* node = BinaryTreeFind(root -> _right,x){
-        if(node){
-            return node;
-        }
-    }
-    return NULL;
-}
+//void DestoryTree(BTNode* root){
+//    if (root == NULL){
+//        return;
+//    }
+//    DestoryTree( root -> _left);
+//    DestoryTree(root -> _right);
+//    free(root);
+//}
