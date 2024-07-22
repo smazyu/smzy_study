e0788edcf1c20690b9996e30e0471052458eff5d
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-23 00:30:15 +0800

    Update question2.c

--- DSStudy/day_19/question2.c
+++ DSStudy/day_19/question2.c
@@ -1,3 +1,23 @@
 //
 // Created by 20212 on 23/7/2024.
 //
+
+BTNode *BinaryTreeFind(BTNode*root,BTDataType x){
+    if(root == NULL){
+        return NULL;
+    }
+    if(root -> _data == x){
+        return root;
+    }
+    BTNode* node = BinaryTreeFind(root -> _left,x){
+        if(node){
+            return node;
+        }
+    }
+    BTNode* node = BinaryTreeFind(root -> _right,x){
+        if(node){
+            return node;
+        }
+    }
+    return NULL;
+}
