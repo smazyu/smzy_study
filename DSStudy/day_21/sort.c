ed9a28e7a6f52407993225309ba2d91f81de1e68
毓 <138857469+smazyu@users.noreply.github.com>
2024-07-28 00:55:21 +0800

    1

--- DSStudy/day_21/sort.c
+++ DSStudy/day_21/sort.c
@@ -77,9 +77,34 @@ void SelectSort(int *a, int n) {
     }
 }//时间复杂度 o(n^2)
 //堆排序
-void AdjustDown(int* a,int n,int root);
+void AdjustDown(int* a,int n,int root){
+    int parent = root;
+    //左孩子
+    int child = parent*2 + 1;
+    while(child < n  ){
+        if(child + 1 < n && a[child + 1] > a[child]){
+            ++child;
+        }
+        if(a[child] > a[parent]){
+            Swap(&a[child],&a[parent]);
+            parent = child;
+            child = parent * 2 + 1;
+        }
+        else{
+            break;
+        }
+    }
+}
 void HeapSort(int *a,int n){
-    
+    for(int i = (n - 1- 1)/2;i >= 0;--i){
+        AdjustDown(a,n,i);
+    }
+    int end = n - 1;
+    while(end > 0){
+        Swap(&a[0],&a[end]);
+        AdjustDown(a,end,0);
+        --end;
+    }
 }
 //希尔排序是对直接插入排序的优化
 //直接插入排序，时间复杂度是 O(n^2) 单趟插入是 O(n)
