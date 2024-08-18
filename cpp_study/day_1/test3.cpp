1e050f99419bc4365386c4cc49125fd002b68abe
毓 <138857469+smazyu@users.noreply.github.com>
2024-08-19 00:26:08 +0800

    1

--- cpp_study/day_1/test3.cpp
+++ cpp_study/day_1/test3.cpp
@@ -8,9 +8,18 @@ using namespace std;
 //函数重载，函数名相同，参数不同
 //对返回值没有要求
 //只是返回值不同，没办法构成重载
-void Func(int i, char ch){};
-void Func(char ch,int i){};
-int main(){
-
-    
+void Func(int i, char ch) {
+    cout << "Func(int, char) called: " << i << " " << ch << endl;
 }
+void Func(char ch, int i) {
+    cout << "Func(char, int) called: " << ch << " " << i << endl;
+}
+
+int main() {
+    Func(1, 'a');  // Corrected this line
+    Func('b', 2);  // Example of using the second overload
+}
+
+//重载:
+//1.什么是函数重载
+//2.c++是如何支持函数重载的，为什么c语言不支持
