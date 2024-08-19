13e3d3a491d818903120e3c8785c2735858a13ea
毓 <138857469+smazyu@users.noreply.github.com>
2024-08-20 01:02:35 +0800

    1

--- cpp_study/day_3/test1.cpp
+++ cpp_study/day_3/test1.cpp
@@ -4,8 +4,32 @@
 //引用是给已经存在的变量取别名
 
 #include <iostream>
+using namespace std;
 int a = 10;
 int& b = a;
 int& c = b;
 int& e = c;
 //引用必须初始化，引用一个实体之后不能再引用别的
+int main(){
+    const int a = 10;
+    const int&b = a;
+    //引用时不能进行权限放大
+    //引用时权限缩小是被允许的
+    return 0;
+}
+
+//1.引用做参数
+void swwap_c(int*p1,int*p2){
+    int tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
+}
+
+void swap_cpp(int&r1,int&r2){
+    int tmp = r1;
+    r1 = r2;
+    r2 = tmp;
+}
+int main(){
+    int a = 0,
+}
