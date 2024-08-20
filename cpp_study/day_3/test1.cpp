46379eb1b17694a6cc10f7980fc1fa802bd816af
毓 <138857469+smazyu@users.noreply.github.com>
2024-08-20 22:48:09 +0800

    1

--- cpp_study/day_3/test1.cpp
+++ cpp_study/day_3/test1.cpp
@@ -5,21 +5,21 @@
 
 #include <iostream>
 using namespace std;
-int a = 10;
-int& b = a;
-int& c = b;
-int& e = c;
-//引用必须初始化，引用一个实体之后不能再引用别的
-int main(){
-    const int a = 10;
-    const int&b = a;
-    //引用时不能进行权限放大
-    //引用时权限缩小是被允许的
-    return 0;
-}
+//int a = 10;
+//int& b = a;
+//int& c = b;
+//int& e = c;
+////引用必须初始化，引用一个实体之后不能再引用别的
+//int main(){
+//    const int a = 10;
+//    const int&b = a;
+//    //引用时不能进行权限放大
+//    //引用时权限缩小是被允许的
+//    return 0;
+//}
 
 //1.引用做参数
-void swwap_c(int*p1,int*p2){
+void swap_c(int*p1,int*p2){
     int tmp = *p1;
     *p1 = *p2;
     *p2 = tmp;
@@ -31,5 +31,8 @@ void swap_cpp(int&r1,int&r2){
     r2 = tmp;
 }
 int main(){
-    int a = 0,
+    int a = 0,b = 1;
+    swap_c(&a,&b);
+    swap_cpp(a,b);
+    return 0;
 }
---
+++ cpp_study/day_4/test1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 20212 on 20/8/2024.
+//
+#include <iostream
+using namespace std;
+
+//引用做返回值
+int main(){
+    return 0;
+}
