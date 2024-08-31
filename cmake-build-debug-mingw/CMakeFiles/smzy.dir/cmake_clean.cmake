9f6ece699b002e919c961d32da37460c20092e6e
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-01 02:03:11 +0800

    1

--- cmake-build-debug-mingw/CMakeFiles/smzy.dir/cmake_clean.cmake
+++ cmake-build-debug-mingw/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -3,6 +3,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/smzy.dir/DSStudy/day_25_review/test2_shellsort.c.obj.d"
   "CMakeFiles/smzy.dir/cpp_study/day_8/text2.cpp.obj"
   "CMakeFiles/smzy.dir/cpp_study/day_8/text2.cpp.obj.d"
+  "CMakeFiles/smzy.dir/cpp_study/day_9/text1.cpp.obj"
+  "CMakeFiles/smzy.dir/cpp_study/day_9/text1.cpp.obj.d"
   "libsmzy.dll.a"
   "smzy.exe"
   "smzy.exe.manifest"
---
+++ cpp_study/day_9/text1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 20212 on 1/9/2024.
+//
+#include <iostream>
+using namespace std;
+
+//class 新增加的关键词
+int main(){
+    return 0;
+}
