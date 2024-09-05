16553aa948a8d435edfb6e9eb7980a3f613e8db8
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-05 23:16:26 +0800

    1

---
+++ cpp_study/day_12/text1.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 20212 on 5/9/2024.
+//
+#include <iostream>
+using namespace std;
+//class Stack{
+//public:
+//    Stack(int n = 10){
+//        _a = (int*)malloc(sizeof(int) * n);
+//        _size = 0;
+//        _capacity = n;//开辟的空间
+//    }
+//    ~Stack(){
+//        free(_a);
+//        _a = nullptr;
+//        _size = _capacity = 0;
+//    }
+//private:
+//    int *_a;
+//    size_t _size;
+//    size_t _capacity;
+//};
+//int main(){
+//    //CPP
+//    Stack st;
+//    //st对象超出作用域，析构函数自动调用
+//
+//}
