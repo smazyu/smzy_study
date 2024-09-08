0dd8566beed215cb3f52a6ebd0fac8ff56d9b447
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-08 23:37:12 +0800

    1

--- cmake-build-debug-mingw/CMakeFiles/smzy.dir/DSStudy/day_25_review/test3_selection_sort.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/smzy.dir/DSStudy/day_25_review/test3_selection_sort.cpp.obj: \
- E:\GitHub\smzy_study\DSStudy\day_25_review\test3_selection_sort.cpp
---
+++ cpp_study/day_13/text1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 20212 on 8/9/2024.
+//
+#include <iostream>
+using namespace std;
+void   operator=(const Data&d){
+    _year = d._year;
+    _month = d._month;
+    _day = d._day;
+}
+return 0;
