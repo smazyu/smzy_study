dea338e2c383470a0cdc72106b34a43bccfe8f37
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-12 14:10:02 +0800

    Update text2.cpp

--- cpp_study/day_14/text2.cpp
+++ cpp_study/day_14/text2.cpp
@@ -1,3 +1,29 @@
 //
 // Created by 20212 on 12/9/2024.
 //
+/*
+ *
+ *
+ *
+ *
+ * */
+
+class Data{
+public:
+    Data(int year = 0, int month = 1,int day = 1){
+        _year = year;
+        _month = month;
+        _day = day;
+    }
+    void print(){
+
+    }
+private:
+    int _year;
+    int _month;
+    int _day;
+};
+int main(){
+    Data d1(2020,4,18);
+    return 0;
+}
