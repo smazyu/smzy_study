ede56f5aa39f9e3fa48601f1f2d88e1b979c68a2
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-16 22:32:37 +0800

    Update text1.cpp

--- cpp_study/day_17/text1.cpp
+++ cpp_study/day_17/text1.cpp
@@ -15,10 +15,17 @@ private:
     int _year = 0;
     int _month = 1;
     int _day = 1;
+    //static int _n 这个不能给初始值 必须在类外面去定义
 };
+//int Date::n = 1;
+void f(Date &d){
+    d._year = 10;
+    cout << d._year <<endl;
+}
 int main(){
     Date d1;
     d1.Print();
     return 0;
 }
 //类里面都是声明
+//友员 类外面访问类里面私有的方式
