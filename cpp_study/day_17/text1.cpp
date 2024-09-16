b20c018621a6173a5c6c0924b2f44f66b2214fa4
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-16 23:29:08 +0800

    Update text1.cpp

--- cpp_study/day_17/text1.cpp
+++ cpp_study/day_17/text1.cpp
@@ -2,14 +2,22 @@
 // Created by 20212 on 16/9/2024.
 //
 #include <iostream>
+
 using namespace std;
-class Date{
-    friend void operator << (ostream&out,const Date& d);
-    friend void f(Date&d);
+
+class Date {
+    friend void operator<<(ostream &out, const Date &d);
+
+    friend void f(Date &d);
+
 public:
-    Date():_year(10){}
-    void Print(){
-        cout <<_year<<"-"<<_month<<"-"<<_day<<endl;
+//    Date():_year(10){}
+//    void Print(){
+//        cout <<_year<<"-"<<_month<<"-"<<_day<<endl;
+//    }
+    Date(int year = 0, int month = 1, int day = 1) : _year(year),
+                                                     _month(month), _day(day) {
+
     }
 
 private:
@@ -20,18 +28,23 @@ private:
     int _day = 1;
     //static int _n 这个不能给初始值 必须在类外面去定义
 };
+
 //int Date::n = 1;
-void operator << (ostream&out,const Date& d){
-    out << d._year <<"/" <<d._month <<"/" << d._day << endl;
+void operator<<(ostream &out, const Date &d) {
+    out << d._year << "/" << d._month << "/" << d._day << endl;
 }
-void f(Date &d){
+
+void f(Date &d) {
     d._year = 10;
-    cout << d._year <<endl;
+    cout << d._year << endl;
 }
-int main(){
+
+int main() {
     Date d1;
-    d1.Print();
+//    d1.Print();
     cout << d1; // operator  << (cout,d1);
+    int i = 0, j = 1;
+    cout << i << j << endl;
     f(d1);
     return 0;
 }
