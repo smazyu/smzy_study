c1b2e391c123f510035cdba53d2c355651e826d1
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-16 22:25:01 +0800

    Update text1.cpp

--- cpp_study/day_17/text1.cpp
+++ cpp_study/day_17/text1.cpp
@@ -3,13 +3,22 @@
 //
 #include <iostream>
 using namespace std;
-class Data{
-    
+class Date{
+public:
+    Date():_year(10){}
+    void Print(){
+        cout <<_year<<"-"<<_month<<"-"<<_day<<endl;
+    }
 private:
-    int _year;
-    int _month;
-    int _day;
+    //c++11
+    //声明时给缺省值
+    int _year = 0;
+    int _month = 1;
+    int _day = 1;
 };
 int main(){
+    Date d1;
+    d1.Print();
     return 0;
 }
+//类里面都是声明
