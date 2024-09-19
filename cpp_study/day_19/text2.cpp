d9146d3bab57c63b51ed4b16d1a0180def78ecfe
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-19 23:52:43 +0800

    1

---
+++ cpp_study/day_19/text2.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 20212 on 18/9/2024.
+//
+//（） 初始化
+class A{
+public:
+
+
+private:
+};
+int main(){
+    int *ptr = new int[3];
+    delete []ptr;
+    return 0;
+}
---
+++ cpp_study/day_20/text1.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 20212 on 19/9/2024.
+//
+#include <iostream>
+using namespace std;
+int main(){
+
+}
