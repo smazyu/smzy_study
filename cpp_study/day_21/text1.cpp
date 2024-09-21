2ef5c169298289ca78ddc94a5f91109753fa99a6
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-21 23:42:58 +0800

    Update text1.cpp

--- cpp_study/day_21/text1.cpp
+++ cpp_study/day_21/text1.cpp
@@ -2,4 +2,29 @@
 // Created by 20212 on 21/9/2024.
 //
 #include <iostream>
+
 using namespace std;
+
+class A {
+public:
+    A() {
+        cout << "A()" << endl;
+    }
+
+    ~A() {
+        cout << "~A()" << endl;
+    }
+
+private:
+    int _a;
+};
+
+int main() {
+    A *p1 = (A *) malloc(sizeof(A));
+    A *p2 = new A;
+    A *p3 = (A *) operator new(sizeof(A));
+
+    //operator new和malloc的区别是什么
+    void* p4 = malloc()
+    return 0;
+}
