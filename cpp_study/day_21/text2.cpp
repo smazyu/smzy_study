f0aae8c1858fa22f037e6a96b38ed870174eaf8a
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-23 00:19:38 +0800

    1

--- cpp_study/day_21/text2.cpp
+++ cpp_study/day_21/text2.cpp
@@ -2,6 +2,7 @@
 // Created by 20212 on 24-9-22.
 //
 #include <iostream>
+
 using namespace std;
 
 //malloc 和 new
@@ -11,8 +12,17 @@ using namespace std;
 
 //c++ 独有的问题 内存泄露 java就轻松很多，因为他有垃圾回收器
 
-int main(){
-    char *p = new char[1024*1024*1024]; //p指向的内存不需要了，忘记或其它原因没有释放就是内存泄露
+int main() {
+//    char *p = new char[1024*1024*1024]; //p指向的内存不需要了，忘记或其它原因没有释放就是内存泄露
     //内存泄露的危害:长期运行的程序(游戏服务),出现内存泄露的危害就很大
+//    return 0;
+    size_t n = 4;
+    try {
+        void *p = new char[n * 1024 * 1024 * 1024];
+        cout << "new:" << p << endl;
+    }
+    catch(exception&e){
+        cout<< e.what() <<endl;
+    }
     return 0;
 }
