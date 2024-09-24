04ffce14a294b25566eba58a16cb5229531d2a7c
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-24 23:53:50 +0800

    1

--- cpp_study/day_23/text1.cpp
+++ cpp_study/day_23/text1.cpp
@@ -6,12 +6,30 @@
 using namespace std;
 
 //1. 4个默认成员函数
-void test_string1 (){
+void test_string1() {
     string s1;
     string s2("hello");
-    string s3("hello",3);
+    string s3("hello", 3);
+    string s4(s2);
+    string s5(s2, 1, 2);
+    string s6(10, 'a');
+    string s7(s2, 1);
+    // string s5()
+    cout << s1 << endl;
+    cout << s2 << endl;
+    cout << s4 << endl;
+    cout << s5 << endl;
+    cout << s6 << endl;
+    cout << s7 << endl;
 }
 
+//npos -> static const size_t npos = -1;无符号整型没有负数，这里的意思是整型的最大值
+
+void test_string2() {
+    string name("John");
+    name += "K.";
+    cout << name << endl;
+}
 int main() {
     test_string1();
     return 0;
