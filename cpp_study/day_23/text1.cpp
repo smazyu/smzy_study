9ca0075a5d6987608b97b50c94ee7349ae97305c
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-24 22:46:29 +0800

    update

--- cpp_study/day_23/text1.cpp
+++ cpp_study/day_23/text1.cpp
@@ -3,3 +3,16 @@
 //
 #include <iostream>
 #include <string>
+using namespace std;
+
+//1. 4个默认成员函数
+void test_string1 (){
+    string s1;
+    string s2("hello");
+    string s3("hello",3);
+}
+
+int main() {
+    test_string1();
+    return 0;
+}
