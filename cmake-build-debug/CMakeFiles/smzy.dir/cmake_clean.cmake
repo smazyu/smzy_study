9b8611d9feba960d16ffc7335d1656f122e9793c
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-25 22:50:09 +0800

    1

--- cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -1,6 +1,6 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/smzy.dir/cpp_study/day_23/text2.cpp.obj"
-  "CMakeFiles/smzy.dir/cpp_study/day_23/text2.cpp.obj.d"
+  "CMakeFiles/smzy.dir/cpp_study/day_23/text3.cpp.obj"
+  "CMakeFiles/smzy.dir/cpp_study/day_23/text3.cpp.obj.d"
   "smzy.exe"
   "smzy.exe.manifest"
   "smzy.lib"
--- cpp_study/day_23/text3.cpp
+++ cpp_study/day_23/text3.cpp
@@ -1,3 +1,50 @@
 //
 // Created by 20212 on 24-9-25.
 //
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void test_string() {
+    string s1("hello world");
+    s1 += ' ';
+    s1 += "world";
+    cout << s1 << endl;
+
+    for (size_t i = 0; i < s1.size(); i++) {
+        cout << s1[i] << " ";
+    }
+    cout << endl;
+    string::iterator it1 = s1.begin();
+    while (it1 != s1.end()) {
+        cout << *it1 << " ";
+        ++it1;
+    }
+    cout << endl;
+
+    for (auto ch: s1) {
+        cout << ch << " ";
+    }
+    cout << endl;
+    //返回c形式的str
+    //返回的是指针
+
+    //获取字符数组首地址，用c字符串形式遍历
+    const char *str = s1.c_str();
+    printf("%s \n", str);
+    while (*str) {
+        cout << *str << " ";
+        ++str;
+    }
+    cout << endl;
+
+    cout << s1 << endl; //调用string重载的operator <<
+    cout << s1.c_str() << endl; //直接输出const char*
+    cout << "-------" << endl;
+
+}
+
+int main() {
+    test_string();
+}
