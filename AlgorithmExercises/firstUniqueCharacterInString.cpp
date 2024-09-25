9ab5e02767e4ec3cb983e8d46661da97cc5717c5
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-25 18:48:58 +0800

    Update firstUniqueCharacterInString.cpp

--- AlgorithmExercises/firstUniqueCharacterInString.cpp
+++ AlgorithmExercises/firstUniqueCharacterInString.cpp
@@ -6,22 +6,22 @@
 using namespace std;
 class Solution {
 public:
-    void characterOccurrences(int chr,int *char_occur){
-        if(chr >= 'a' && chr <= 'z'){
-            char_occur[chr - 'a'] ++;
+    void characterOccurrences(int chr, int (&char_occur)[26]) {
+        if (chr >= 'a' && chr <= 'z') {
+            char_occur[chr - 'a']++;
         }
     }
     int firstUniqChar(string s) {
         int char_occur[26] = {0};
-        for(auto c: s) {
-            characterOccurrences(c,char_occur);
+        for (auto c : s) {
+            characterOccurrences(c, char_occur);
         }
-        for(int i = 0;i < s.size();i++) {
-            if(char_occur[s[i] - 'a'] == 1) {
+        for (int i = 0; i < s.size(); i++) {
+            if (char_occur[s[i] - 'a'] == 1) {
                 return i;
-            }else {
-                return -1;
             }
+
         }
+        return -1;
     }
 };
