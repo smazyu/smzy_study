8f2e005cbf892cefc0c8ad74cda7fbb6ec410697
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-26 20:11:50 +0800

    1

--- cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -1,6 +1,6 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/smzy.dir/cpp_study/day_24/text1.cpp.obj"
-  "CMakeFiles/smzy.dir/cpp_study/day_24/text1.cpp.obj.d"
+  "CMakeFiles/smzy.dir/cpp_study/day_24/text2.cpp.obj"
+  "CMakeFiles/smzy.dir/cpp_study/day_24/text2.cpp.obj.d"
   "smzy.exe"
   "smzy.exe.manifest"
   "smzy.lib"
