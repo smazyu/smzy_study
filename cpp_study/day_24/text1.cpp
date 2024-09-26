beda5772eb1d6534d54a28f11841173e9ca19bb6
毓 <138857469+smazyu@users.noreply.github.com>
2024-09-26 15:40:19 +0800

    1

--- cpp_study/day_24/text1.cpp
+++ cpp_study/day_24/text1.cpp
@@ -1,41 +1,35 @@
-//
-// Created by 20212 on 24-9-26.
-//
-//编码的本质是映射
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 void test_string() {
-    string s1("string.cpp.zip");
-    string s2("string.c");
-    string s3("string.txt");
+    // 用于存储测试字符串
+    string testStrings[] = {"string.cpp.zip", "string.c", "string.txt"};
 
-    size_t pos1 = s1.rfind('.');
-    if (pos1 != string::npos) {
-        cout << s1.substr(pos1) << endl;
+    // 遍历每个字符串，提取并输出后缀
+    for (const auto &str : testStrings) {
+        size_t pos = str.rfind('.');
+        if (pos != string::npos) {
+            cout << str.substr(pos) << endl;
+        }
     }
 
-    size_t pos2 = s2.find('.');
-    if (pos2 != string::npos) {
-        cout << s2.substr(pos2) << endl;
-    }
-
-    size_t pos3 = s3.find('.');
-    if (pos3 != string::npos) {
-        cout << s3.substr(pos3) << endl;
-    }
-
+    // 解析 URL
     string url("https://blog.simazhangyu.com/");
-    size_t i1 = url.find(':');
-    if (i1 != string::npos) {
-        cout << url.substr(0, i1) << endl;
+    size_t scheme_end = url.find(':'); // 查找 ':' 的位置
+    if (scheme_end != string::npos) {
+        cout << url.substr(0, scheme_end) << endl; // 输出协议部分
     }
-    size_t i2 = url.find('/', i1 + 3);
-    if (i2 != string::npos) {
-        cout << url.substr(i1 + 3, i2 - i1 - 3) << endl;
+
+    size_t host_start = scheme_end + 3; // 跳过 '://'
+    size_t path_start = url.find('/', host_start);
+    if (path_start != string::npos) {
+        cout << url.substr(host_start, path_start - host_start) << endl; // 输出主机名部分
     }
-    cout << url.substr(i2 + 1) << endl;
+
+    // 输出路径部分
+    cout << url.substr(path_start + 1) << endl;
 }
 
 int main() {
