d909c49a89f90aac44f3cce9d9225aaefc74939e
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-09 17:44:23 +0800

    Update vector.h

--- cpp_study/day_29_vector_2/vector.h
+++ cpp_study/day_29_vector_2/vector.h
@@ -4,20 +4,41 @@
 template<class T>
 class vector {
 public:
-    typedef T* iterator;
+    typedef T *iterator;
 
-    vector():_start(nullptr),_finish(nullptr),_endofstorage() {
-
+    vector(): _start(nullptr), _finish(nullptr), _endofstorage() {
     };
-    void push_back(const T& x) {
 
+    void reserve(size_t n) {
+        T *tmp = new T[n];
+        if (n > capacity()) {
+            if (_start) {
+                memcpy(tmp, _start, sizeof(T) * capacity());
+                delete[] _start;
+            }
+        }
+        _start = tmp;
+        _finish = _start + size();
+        _endofstorage = _start + capacity();
     }
+
+    void push_back(const T &x) {
+        if (_finish = _endofstorage) {
+            size_t newcapacity = capacity() == 0 ? 2 : capacity() * 2;
+            reserve(newcapacity);
+        }
+        *_finish = x;
+        ++_finish;
+    }
+
     size_t size() {
         return _finish - _start;
     }
+
     size_t capacity() {
         return _endofstorage - _finish;
     }
+
 private:
     iterator _start;
     iterator _finish;
