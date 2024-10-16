c29bce257483955b0a3553c2c778004e5792b2bc
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-16 23:14:47 +0800

    1

--- cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -1,13 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/smzy.dir/cpp_study/day_31_list/test1.cpp.obj"
-  "CMakeFiles/smzy.dir/cpp_study/day_31_list/test1.cpp.obj.d"
-  "libsmzy.dll.a"
-  "smzy.exe"
-  "smzy.exe.manifest"
-  "smzy.pdb"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/smzy.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
--- cpp_study/day_31_list/lish.h
+++ cpp_study/day_31_list/lish.h
@@ -1,9 +1,34 @@
 //
 // Created by 20212 on 24-10-16.
 //
+//一个类型去封装节点的指针构成一个自定义类型
+//然后重载*,++等运算符,就可以达到我们的行为
 #pragma once
 namespace study_list
 {
-
-
+    template <class T>
+    struct __list_node
+    {
+        __list_node<T>* prev;
+        __list_node<T>* next;
+        T _data;
+    };
+    template <class T>
+    class list
+    {
+        typedef __list_node<T> node;
+        public:
+        //带头双向循环链表
+        list()
+        {
+            _head = new node();
+            _head-> _next = _head;
+            _head-> _prev = _head;
+        }
+    };
 }
+
+//迭代器失效总结
+//1.vector的迭代器失效 iterator insert erase都会导致失效
+//push_back增容失效
+//2.list的iterator,erase会导致失效
