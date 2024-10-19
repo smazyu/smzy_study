041e1134debdd648aa443b9d0c8d7d23f3b2984c
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-19 23:36:56 +0800

    Update stack.h

--- cpp_study/day_33_stack_and_queue/stack.h
+++ cpp_study/day_33_stack_and_queue/stack.h
@@ -4,3 +4,21 @@
 //适配器模式
 //生活中我们使用的适配器是用来进行电压转化的
 
+//stack queue 叫做容器适配器
+//通过容器转换出来的
+
+#pragma once
+namespace stack_study
+{
+    template<class T,class Container>
+    class stack
+    { 
+    public:
+        void push(const T& x)
+        {
+            _con.push_back(x);
+        }
+    private:
+        Container _con;
+    };
+}
