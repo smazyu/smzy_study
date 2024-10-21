fa282bb6ef144b0e5b8882361cc3275fd4ffdcb3
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-21 20:58:29 +0800

    1

--- cpp_study/day_34_stack_and_queue_2/test.cpp
+++ cpp_study/day_34_stack_and_queue_2/test.cpp
@@ -1,3 +1,12 @@
 //
 // Created by 20212 on 24-10-21.
 //
+//deque 迭代器非常复杂
+//iterator it = d.begin()
+//while(it != d.end(){
+//cout <<*it<<" ";
+//++it;
+//} cout << endl;
+
+//deque缺点：1.大量频繁的operator[]的效率低
+//
