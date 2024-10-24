62989e3d667b48fbd2cd216a7decddf5a1412079
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-24 16:16:27 +0800

    1

--- cpp_study/day_35_advanced_templates/Func.cpp
+++ cpp_study/day_35_advanced_templates/Func.cpp
@@ -6,8 +6,13 @@ void F1()
 {
     cout << " F1()" << endl;
 }
-
-void F2()
+template<class T>
+void F2(const T&x)
 {
-    cout << "F2()" << endl;
+    cout << "F2(const T& x)" << endl;
 }
+//解决模板不能分离编译的方法
+//1.显示实例化 不常用，因为不方便
+//2.不要分离编译了
+template
+void F2<int>(const int& x);
