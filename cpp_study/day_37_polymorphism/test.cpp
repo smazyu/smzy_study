4c48bc8ae137c782c6448968bf4f4fa286968cbd
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-28 01:06:43 +0800

    1

--- cpp_study/day_37_polymorphism/test.cpp
+++ cpp_study/day_37_polymorphism/test.cpp
@@ -7,22 +7,22 @@
 //构成多态 必须通过基类的指针或者引用调用虚函数 被调用的函数
 #include <iostream>
 using namespace std;
-class Person
-{
-public:
-    virtual void BuyTicket()
-    {
-        cout << "买票全价"<< endl;
-    }
-private:
-    int _a;
-    char _ch;
-};
-int main()
-{
-//virtual关键字，可以修饰成原函数，为了完成虚函数的重写，满足多态的条件之一
-
-}
+// class Person
+// {
+// public:
+//     virtual void BuyTicket()
+//     {
+//         cout << "买票全价"<< endl;
+//     }
+// private:
+//     int _a;
+//     char _ch;
+// };
+// int main()
+// {
+// //virtual关键字，可以修饰成原函数，为了完成虚函数的重写，满足多态的条件之一
+//
+// }
 //在成员函数前面加一个virtual的是虚函数
 
 //1.满足多态的条件和对象有关，指向哪个对象就调用他的虚函数
@@ -41,3 +41,25 @@ int main()
 //1.协变 基类虚函数返回基类对象的指针或引用
 //       派生类虚函数返回派生类对象的指针或引用
 //2.重写的时候，派生类不加virtual关键字
+class Person
+{
+public:
+    virtual ~Person()
+    {
+        cout <<"~Person"<<endl;
+    }
+};
+class Student:public Person
+{
+public:
+    virtual ~Student()
+    {
+        cout << "~Student" <<endl;
+    }
+};
+int main()
+{
+    Person p;
+    Student s;
+    return 0;
+}
