7ae8560166acdfc6eabb8d9002250bfc8bfae01c
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-28 02:44:12 +0800

    1

--- cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/smzy.dir/cpp_study/day_37_polymorphism/test.cpp.obj"
   "CMakeFiles/smzy.dir/cpp_study/day_37_polymorphism/test.cpp.obj.d"
+  "CMakeFiles/smzy.dir/cpp_study/day_37_polymorphism/test1.cpp.obj"
+  "CMakeFiles/smzy.dir/cpp_study/day_37_polymorphism/test1.cpp.obj.d"
   "libsmzy.dll.a"
   "smzy.exe"
   "smzy.exe.manifest"
