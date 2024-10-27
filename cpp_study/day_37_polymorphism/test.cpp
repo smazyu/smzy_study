d16bb633268700a654acf91aee2d67a7b13c5d1d
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-28 02:36:33 +0800

    1

--- cpp_study/day_37_polymorphism/test.cpp
+++ cpp_study/day_37_polymorphism/test.cpp
@@ -44,32 +44,62 @@ using namespace std;
 class Person
 {
 public:
-     virtual ~Person()
+    virtual ~Person()
     //析构函数的函数名会被处理成destructor
     {
-        cout <<"~Person"<<endl;
+        cout << "~Person" << endl;
     }
 };
-class Student:public Person
+
+class Student : public Person
 {
 public:
     virtual ~Student()
     //析构函数的函数名会被处理成destructor
     {
-        cout << "~Student" <<endl;
+        cout << "~Student" << endl;
     }
 };
-int main()
-{
-    Person * p1 = new Person;
-    delete p1;
-//重写是对函数体重写
-    Person* p2 = new Student;
-    delete p2;
-    return 0;
-}
+
+// int main()
+// {
+//     Person * p1 = new Person;
+//     delete p1;
+// //重写是对函数体重写
+//     Person* p2 = new Student;
+//     delete p2;
+//     return 0;
+// }
 //面试题 ：析构函数是否需要定义成虚函数?
 //不构成多态：
 //调用的指针类型是谁就调用谁的析构函数
 //构成多态
 //调用的指针指向谁就调用谁的析构函数
+
+//final关键字代表不能被重写
+//final类不能被继承
+// override 检查子类的虚函数是否重写了父类的虚函数
+//重载 两个函数在同一作用域 函数名相同，参数不同
+//重写 两个函数分别在基类和派生类的作用域，函数名，参数，返回值相同，两个函数都是虚函数 重写是为了
+//重写函数实现
+//重定义 两个基类和派生类的同名函数不构成重写就是重定义
+
+//抽象类的特点 不能实例化出对象
+class Car
+{
+public:
+    virtual void Drive() = 0; //不需要实现，纯虚函数
+};
+
+class Benz:public Car
+{
+public:
+    virtual void Drive(){};
+};
+int main()
+{
+    // Car car; //抽象类不能实例化出对象
+    return 0;
+}
+//1.纯虚函数的作用，强制子类去完成重写
+//2.表示抽象的类型。 抽象就是在现实中没有对应的实体的。
