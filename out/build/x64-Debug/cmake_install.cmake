5418ceda35cc6c0cf5a504a37ac2a99eb882f407
毓 <138857469+smazyu@users.noreply.github.com>
2024-10-29 11:11:29 +0800

    1

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: E:/GitHub/smzy_study
+# Install script for directory: C:/Users/20212/Desktop/smzy_study
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "E:/GitHub/smzy_study/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/20212/Desktop/smzy_study/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/GitHub/smzy_study/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/20212/Desktop/smzy_study/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
