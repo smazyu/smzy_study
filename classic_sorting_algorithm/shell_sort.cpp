f43bbf7ba648fe16352a1a55dbd55507fb3e555d
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-01 01:20:56 +0800

    1

--- classic_sorting_algorithm/shell_sort.cpp
+++ classic_sorting_algorithm/shell_sort.cpp
@@ -1,3 +1,20 @@
 //
 // Created by 20212 on 24-11-1.
 //
+void shell_sort(int arr[], int n)
+{
+    int i, j, inc, key;
+    //初始增量:n/2,每一趟之后除以二
+    for (inc = n / 2; inc > 0; inc /= 2)
+    {
+        for (i = inc; i < n; i++)
+        {
+            key = arr[i];
+            for (j = i; j >= inc && key < arr[j - inc]; j -= inc)
+            {
+                arr[j] = arr[j - inc];
+            }
+            arr[j] = key;
+        }
+    }
+}
