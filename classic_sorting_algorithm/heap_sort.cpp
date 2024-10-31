4e7af0c82237df84de54c9b129dfe75a1da3b7ad
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-01 00:34:38 +0800

    1

--- classic_sorting_algorithm/heap_sort.cpp
+++ classic_sorting_algorithm/heap_sort.cpp
@@ -1,3 +1,28 @@
 //
 // Created by 20212 on 24-11-1.
 //
+//下标为i的节点的父节点(i-1)/2
+//左孩子 i*2 + 1;
+//右孩子 i*2 + 2;
+void swap(int*a,int*b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int lson = i * 2 + 1;
+    int rson = i * 2 + 2;
+    if (lson < n && arr[largest] < arr[lson])
+        largest = lson;
+    if (rson < n && arr[largest] < arr[rson])
+        largest = rson;
+    if (largest != i)
+    {
+        swap(&arr[largest], &arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+void 
