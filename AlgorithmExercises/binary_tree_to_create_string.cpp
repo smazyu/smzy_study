4ace1c5f7fb68ff6716e8b0e41c3317d2ee614a6
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-05 23:49:48 +0800

    1

--- AlgorithmExercises/binary_tree_to_create_string.cpp
+++ AlgorithmExercises/binary_tree_to_create_string.cpp
@@ -4,3 +4,30 @@
 /*给你二叉树的根节点 root ，请你采用前序遍历的方式，将二叉树转化为一个由括号和整数组成的字符串，返回构造出的字符串。
 
 空节点使用一对空括号对 "()" 表示，转化后需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。*/
+using  namespace std;
+#include <iostream>
+#if 0
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        TreeNode* t = root;
+        string str;
+        if(t == NULL)
+            return str;
+        str += to_string(t->val);
+        if(t -> left){
+            str += '(';
+            str += tree2str(t -> left);
+            str += ')';
+        }else if(t -> right){
+            str += "()";
+        }
+        if(t -> right){
+            str += '(';
+            str += tree2str(t->right);
+            str += ')';
+        }
+        return str;
+    }
+};
+#endif
