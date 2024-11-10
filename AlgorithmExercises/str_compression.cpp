dc1cad580a4d98be6a6ddfa05a86edd1d39d1fdd
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-10 22:28:17 +0800

    1

--- AlgorithmExercises/str_compression.cpp
+++ AlgorithmExercises/str_compression.cpp
@@ -1,3 +1,15 @@
-//
-// Created by 20212 on 24-11-10.
-//
+class Solution {
+public:
+    string compressString(string S) {
+        string comp_str;
+        int r = 0, l = 0, n = S.size();
+        while (l < n) {
+            while(r < n && S[l] == S[r]){
+                r++;}
+            comp_str += S[l] + to_string(r - l);
+            l = r;
+        }
+        // return S.size() > comp_str.size() ? comp_str : S;
+        return comp_str.size() >= S.size() ? S : comp_str;
+    }
+};
