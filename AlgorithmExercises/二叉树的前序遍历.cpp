5e13886c60436da23555c375bf38236228888521
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-11 23:11:38 +0800

    Update 二叉树的前序遍历.cpp

--- AlgorithmExercises/二叉树的前序遍历.cpp
+++ AlgorithmExercises/二叉树的前序遍历.cpp
@@ -1,1 +1,10 @@
 //  给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+
+struct TreeNode
+{
+    int val;
+    *TreeNode *left;
+    *TreeNode *right;
+    *TreeNode() : val(0), left(nullptr), right(nullptr){} * TreeNode(int x) : val(x), left(nullptr), right(nullptr){} * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+                                                                                                                                                                                                   *
+};
