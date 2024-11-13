66b640c794cc781bd1cda2b5d88d0ebfd7fd5dcc
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-13 18:10:28 +0800

    1

--- cpp_study/day_40_map_set/test1.cpp
+++ cpp_study/day_40_map_set/test1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <set>
 #include <map>
+#include <string>
 using namespace std;
 void test_set1()
 {
@@ -57,6 +58,7 @@ void test_map1()
     m.insert(pair<int, int>(2, 2));
     m.insert(pair<int, int>(3, 3));
     m.insert(make_pair(4, 4)); // 函数模板构造一个对象
+    // 大家都喜欢用make_pair，因为是由编译器自己推演
 
     map<int, int>::iterator it = m.begin();
     while (it != m.end())
@@ -74,10 +76,51 @@ void test_map1()
     }
     cout << endl;
 }
+
+void test_map2()
+{
+    std::map<std::string, std::string> dict;
+    dict.insert(pair<std::string, std::string>("sort", "排序"));
+    dict.insert(make_pair("string", "排序"));
+
+    std::map<std::string, std::string>::iterator it = dict.begin();
+    while (it != dict.end())
+    {
+        cout << it->first << ":" << it->second << " ";
+        ++it;
+    }
+    cout << endl;
+}
+
+void test_map3()
+{
+    string strs[]{"西瓜", "西瓜", "西瓜", "西瓜", "西瓜", "西瓜", "西瓜"};
+    map<string, int> coutMap;
+    for (auto &str : strs)
+    {
+        map<string, int>::iterator ret = coutMap.find(str);
+        if (ret != coutMap.end())
+        {
+            ret->second++;
+        }
+        else
+        {
+            coutMap.insert(make_pair(str, 1));
+        }
+    }
+
+    for (auto &e : coutMap)
+    {
+        cout << e.first << " : " << e.second << " ";
+    }
+    cout << endl;
+}
 int main()
 {
     // test_set1();
-    test_map1();
+    // test_map1();
+    // test_map2();
+    test_map3();
 }
 
 // 如果把中国所有人的身份存入set中，查找一个人在不在最多31次
