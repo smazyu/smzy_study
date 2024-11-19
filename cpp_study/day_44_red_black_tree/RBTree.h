98cd92642d823f88e082fab42d102b5f29846b4a
毓 <138857469+smazyu@users.noreply.github.com>
2024-11-19 23:54:35 +0800

    1

---
+++ cpp_study/day_44_red_black_tree/RBTree.h
@@ -0,0 +1,66 @@
+#pragma once
+enum Colour
+{
+    BLACK,
+    RED,
+};
+template <class K, class V>
+class RBTreeNode
+{
+    RBTreeNode<K, V> *_left;
+    RBTreeNode<K, V> *_right;
+    RBTreeNode<K, V> *_parent;
+
+    pair<K, V> _kv;
+    Colour _col;
+};
+
+template <class K, class V>
+class RBTree
+{
+    typedef RBTreeNode<K, V> Node;
+
+public:
+    bool Insert(const pair<K, V> &kv)
+    {
+        if (_root == nullptr)
+        {
+            _root = new Node(kv);
+            return true;
+        }
+        Node *parent = nullptr;
+        Node *cur = _root;
+        while (cur)
+        {
+            if (cur->_kv.first < kv.first)
+            {
+                parent = cur;
+                cur = cur->_right;
+            }
+            else if (cur->_kv.first > kv.first)
+            {
+                parent = cur;
+                cur = cur->_left;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        cur = new Node(kv);
+        if (parent->_kv.first < kv.first)
+        {
+            parent->_right = cur;
+            cur->_parent = parent;
+        }
+        else
+        {
+            parent->_left = cur;
+            cur->_parent = parent;
+        }
+        return true;
+    };
+
+private:
+    Node *_root = nullptr;
+}
