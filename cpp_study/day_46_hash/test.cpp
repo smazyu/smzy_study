0c2e1de89ad708863bebb6fceebc331465759555
smazyu <138857469+smazyu@users.noreply.github.com>
2024-12-01 23:45:17 +0800

    2

--- cpp_study/day_46_hash/test.cpp
+++ cpp_study/day_46_hash/test.cpp
@@ -5,8 +5,26 @@
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
+
+void test_unordered_map_set()
+{
+    unordered_set<int> us;
+    us.insert(4);
+    us.insert(3);
+    us.insert(1);
+    us.insert(7);
+
+    unordered_set<int>:: iterator it = us.begin();
+    while (it != us.end())
+    {
+        cout << *it << " ";
+        ++ it;
+    }
+    cout << endl;
+}
 int main()
 {
+    test_unordered_map_set();
     return 0;
 }
 // unordered map
