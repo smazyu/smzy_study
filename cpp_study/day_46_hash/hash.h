c581d231b8b87cf03d91976f71cf7eea4832f5cc
smazyu <138857469+smazyu@users.noreply.github.com>
2024-12-02 04:30:43 +0800

    Update hash.h

--- cpp_study/day_46_hash/hash.h
+++ cpp_study/day_46_hash/hash.h
@@ -24,23 +24,71 @@ template <class K, class T>
 class HashTable
 {
 public:
+    // int KeyOfT(const T& d)
+    // {
+    //     if (typeof(K) == typeof(T))
+    //     {
+    //         return
+    //     }
+    // }
     bool Insert(const T& d)
     {
+        KeyOfT kt;
         //计算d中的key在表中映射的位置
-        size_t index = d % _tables.size();
+        size_t index = kt(d) % _tables.size();
         while (_tables[index]._state == EXIST)
         {
+            if (_tables[index]._data == kt(d))
+            {
+                return false;
+            }
             ++index;
             if (index == _tables.size())
             {
                 index = 0;
             }
         }
-        _tables[index]._data = d;
+        _tables[index]._data = kt(d);
         _tables[index]._state = EXIST;
         _nums++;
+        return true;
     }
 
+    HashData* Find(const K& key)
+    {
+        KeyOfT kt;
+        size_t index = key % _tables.size();
+        while (_tables[index]._state != EMPTY)
+        {
+            if (kt(_tables[index]._data) == key)
+            {
+                if (_tables[index]._state == EXIST)
+                {
+                    return &_tables[index];
+                }
+                else
+                {
+                    return nullptr;
+                }
+                return &_tables[index];
+            }
+            ++index;
+            if (index == _tables.size())
+            {
+                index = 0;
+            }
+        }
+    }
+    bool Erase(const K& key)
+    {
+        HashData* ret = Find(key);
+        if (ret)
+        {
+            ret->_state = DELETE;
+            return true;
+        }
+        return false;
+    }
 private:
     vector<T> _tables;
     size_t _num; //存了几个有效数据
