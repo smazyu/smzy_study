c991af2fd5acb8f284f337b97c8d355d2ad84561
smazyu <138857469+smazyu@users.noreply.github.com>
2024-12-07 23:29:59 +0800

    Update test.cpp

--- cpp_study/day_49_bitmap_and_bloom_filters/test.cpp
+++ cpp_study/day_49_bitmap_and_bloom_filters/test.cpp
@@ -3,12 +3,21 @@
 //
 //位图 既节省空间 效率又高
 #pragma once
+#include <vector>
 namespace bit
 {
     class bitset
     {
     public:
+        bitset(size_t N)
+        {
+            //位图应该存位
+            _bits.resize(N/32 + 1,0);
+            _num = 0;
+        }
     private:
-        
+        // int* _bits;
+        std::vector<int> _bits;
+        size_t _num;//映射存储的多少个数据
     };
 }
