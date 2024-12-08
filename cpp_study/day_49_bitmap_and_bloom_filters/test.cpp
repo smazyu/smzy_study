e7d5a098b999a84e3db9ef0d958b1ebc1ba64b9e
smazyu <138857469+FluffyBloomX@users.noreply.github.com>
2024-12-09 00:15:43 +0800

    q

--- cpp_study/day_49_bitmap_and_bloom_filters/test.cpp
+++ cpp_study/day_49_bitmap_and_bloom_filters/test.cpp
@@ -16,7 +16,7 @@ namespace bit
             //一个整形有32位 所以N/32就可以存N个数据 因为是按位存储的
             _bits.resize(N/32 + 1,0);
             _num = 0;
-            
+
         }
         void set(size_t x)
         {
@@ -24,6 +24,7 @@ namespace bit
             size_t pos = x%32;//算出x映射的位置在整形的第几位
 //左移是向高位移 右移是向低位移
             _bits[index] |= (1<<pos);//将第pos位置1 |= 是或等于 1<<pos是将1左移pos位 异或是不同为1 相同为0 这里用异或将第pos位置1
+            ++_num;
         }
 
         void reset(size_t x)
@@ -31,6 +32,7 @@ namespace bit
             size_t index = x/32;
             size_t pos = x%32;
             _bits[index] &= ~(1<<pos);//将第pos位置0 &=是与等于 ~(1<<pos)是将1左移pos位取反 0的取反是1 1的取反是0 与是同为1才为1 不然就是0
+            --_num;
         }
 
         bool test (size_t x)
